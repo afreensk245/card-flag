01fdaa9454c33d7f06413e6d79a6665e4a9ea3b9
afreensk245 <117528491+afreensk245@users.noreply.github.com>
2022-11-30 14:37:24 +0530

    2nd commit
    country flag task

---
+++ assets/js/app.js
@@ -0,0 +1,119 @@
+
+
+var cl=console.log
+cl(counyries);
+const countries = [
+  {
+    name: "Afghanistan",
+    capital: "Kabul",
+    languages: [
+      "Pashto",
+      "Uzbek",
+      "Turkmen"
+    ],
+    population: 38928341,
+    flag: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg",
+    currencies: [
+      {
+        code: "AFN",
+        name: "Afghan afghani",
+        symbol: "؋"
+      }
+    ]
+  },
+  {
+    name: "Åland Islands",
+    capital: "Mariehamn",
+    languages: [
+      "Swedish"
+    ],
+    population: 28875,
+    flag: "https://flagcdn.com/ax.svg",
+    currencies: [
+      {
+        code: "EUR",
+        name: "Euro",
+        symbol: "€"
+      }
+    ]
+  },
+   {
+    name: "Albania",
+    capital: "Tirana",
+    languages: [
+      "Albanian"
+    ],
+    population: 2837743,
+    flag: "https://flagcdn.com/al.svg",
+    currencies: [
+      {
+        code: "ALL",
+        name: "Albanian lek",
+        symbol: "L"
+      }
+    ]
+  },
+  {
+    name: "Algeria",
+    capital: "Algiers",
+    languages: [
+      "Arabic"
+    ],
+    population: 43851043,
+    flag: "https://flagcdn.com/dz.svg",
+    currencies: [
+      {
+        code: "DZD",
+        name: "Algerian dinar",
+        symbol: "د.ج"
+      }
+    ]
+  },
+  {
+    name: "Andorra",
+    capital: "Andorra la Vella",
+    languages: [
+      "Catalan"
+    ],
+    population: 77265,
+    flag: "https://flagcdn.com/ad.svg",
+    currencies: [
+      {
+        code: "EUR",
+        name: "Euro",
+        symbol: "€"
+      }
+    ]
+  },
+  {
+    name: "Angola",
+    capital: "Luanda",
+    languages: [
+      "Portuguese"
+    ],
+    population: 32866268,
+    flag: "https://flagcdn.com/ao.svg",
+    currencies: [
+      {
+        code: "AOA",
+        name: "Angolan kwanza",
+        symbol: "Kz"
+      }
+    ]
+  },
+  {
+    name: "Anguilla",
+    capital: "The Valley",
+    languages: [
+      "English"
+    ],
+    population: 13452,
+    flag: "https://flagcdn.com/ai.svg",
+    currencies: [
+      {
+        code: "XCD",
+        name: "East Caribbean dollar",
+        symbol: "$"
+      }
+    ]
+  },
